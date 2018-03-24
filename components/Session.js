a41c3c0eb79385fb2586d6222382f27f8b1b83a7
Remy Sharp <remy@remysharp.com>
2018-03-24 16:53:12 +0000

    Custom server to handle GET /session/:slug

--- components/Session.js
+++ components/Session.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default ({ title, description, slug }) => (
   <div className="Session">
     <h2>
-      <Link href={`/session?slug=${slug}`}>
+      <Link as={`/session/${slug}`} href={`/session?slug=${slug}`}>
         <a>{title}</a>
       </Link>
     </h2>
