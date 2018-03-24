1317f5e904209088a5ae87a1f00e43bc114d637c
Remy Sharp <remy@remysharp.com>
2018-03-24 14:06:16 +0000

    Page transitions now include a progress loader
    A huge help with UX and letting your user know that *something* has
    happened and that your software hasn't broken.

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,11 +1,21 @@
 import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+
+Router.onRouteChangeStart = url => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
 
 export default ({ children, title = 'Nextconf Schedule' }) => (
   <div id="root">
     <Head>
       <title>{title}</title>
+      <link rel="stylesheet" href="/static/nprogress.css" />
     </Head>
     <Header />
     <main>{children}</main>
