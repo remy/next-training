86123391214d897f610ba0dac360b2a981032a7a
Remy Sharp <remy@remysharp.com>
2018-03-25 15:54:18 +0100

    How to use bundle analyser for client & server

--- next.config.js
+++ next.config.js
@@ -2,11 +2,35 @@ require('@remy/envy');
 const webpack = require('webpack');
 const withCSS = require('@zeit/next-css');
 
-module.exports = withCSS({
-  cssModules: false,
-  webpack: config => {
-    config.plugins.push(new webpack.EnvironmentPlugin(['SHOW_SPEAKER']));
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 
-    return config;
-  },
-});
+module.exports = withCSS(
+  withBundleAnalyzer({
+    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: 'static',
+        reportFilename: '../../bundles/server.html',
+      },
+      browser: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/client.html',
+      },
+      openAnalyzer: true,
+    },
+    cssModules: false,
+    webpack: config => {
+      config.plugins.push(
+        new webpack.EnvironmentPlugin([
+          'SHOW_SPEAKER',
+          'PORT',
+          'API',
+          'NOW_URL',
+        ])
+      );
+
+      return config;
+    },
+  })
+);
