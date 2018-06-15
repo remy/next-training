22ca5e46a4cb6ceff781c8c7ca45441835c32a1d
Remy Sharp <remy@remysharp.com>
2018-06-15 13:43:19 +0100

    Polyfill done right 💪

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width" />
           <link rel="stylesheet" href="/_next/static/style.css" />
           <link rel="stylesheet" href="/static/styles.css" />
+          <script src="https://cdn.polyfill.io/v2/polyfill.min.js" />
         </Head>
         <body>
           <Main />
